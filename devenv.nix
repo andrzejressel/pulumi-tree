6de99e780840ddfdf2fdb69957594fa2159eee64
Andrzej Ressel <github@andrzejressel.pl>
2024-09-01 19:49:05 +0200

    Update

--- devenv.nix
+++ devenv.nix
@@ -11,6 +11,8 @@
 
   languages.go.enable = true;
   languages.go.enableHardeningWorkaround = true;
+  languages.java.enable = true;
+  languages.java.gradle.enable = true;
   languages.javascript.enable = true;
   languages.javascript.yarn.enable = true;
 }
